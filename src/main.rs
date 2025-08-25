68994b053f0ebe6d4cd8dbdbf21b5c6ab8ad4ec8
Aryangitrepo <aryanpatkar99@gmail.com>
2025-08-25 06:33:36 +0530

    build

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ mod utils;
 async fn main() {
     let app= route::v1::router_new();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("server started");
     axum::serve(listener, app).await.unwrap();
-
 }
